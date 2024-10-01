e8ef2beee7c89485ed59cae44d14d42356801f51
alessmcs <alessandramancas23@gmail.com>
2024-10-01 13:40:26 -0400

    test 1 comparePaths

--- core/src/test/java/com/graphhopper/util/GHUtilityTest.java
+++ core/src/test/java/com/graphhopper/util/GHUtilityTest.java
@@ -19,6 +19,7 @@ package com.graphhopper.util;
 
 
 import com.graphhopper.coll.GHIntLongHashMap;
+import com.graphhopper.routing.Path;
 import com.graphhopper.routing.ev.BooleanEncodedValue;
 import com.graphhopper.routing.ev.DecimalEncodedValue;
 import com.graphhopper.routing.ev.DecimalEncodedValueImpl;
@@ -31,6 +32,9 @@ import com.graphhopper.storage.NodeAccess;
 import com.graphhopper.storage.*;
 import com.graphhopper.routing.ch.CHRoutingAlgorithmFactory;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.graphhopper.util.GHUtility.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -161,6 +165,77 @@ public class GHUtilityTest {
         } catch (IllegalStateException e) {
             System.out.println("Excepted exception is handled");
         }
+    }
+
+    /*
+    Nous avons observé que comparePaths, dans ses conditions, ne prend pas en compte qu'un
+    Path pourrait avoir un poids, une distance ou un temps nuls.
+    Même si, actuellement, la méthode est appelée dans le cadre d'un test (où les valeurs sont
+    non-nulles car les objets sont correctement initialisés), il faut quand même qu'elle les
+    prenne en compte. Il se peut qu'à un moment donné, un utilisateur fait rouler comparePaths
+    sans bien initialiser ses objets, et la méthode devrait le laisser savoir.
+ */
+    @Test
+    public void testComparePaths_edgeCases(){
+
+        final long seed = System.nanoTime();
+
+        // Graph graph = initGraph_forComparePaths();
+
+        Directory dir_p1 = new RAMDirectory();
+        BaseGraph graph_p1 = new BaseGraph(dir_p1, true, true, 100, 8);  // Création de l'instance de BaseGraph
+        graph_p1.create(500);
+        NodeAccess na_p1 = graph_p1.getNodeAccess();
+        na_p1.setNode(1, 10, 10, 0); na_p1.setNode(2, 8, 10, 0);
+        na_p1.setNode(3, 8, 7,0); na_p1.setNode(5, 5, 7,0);
+        graph_p1.edge(1,2).setDistance(2);
+        graph_p1.edge(2,3).setDistance(4);
+        graph_p1.edge(3,4).setDistance(1);
+
+        Directory dir_p2 = new RAMDirectory();
+        BaseGraph graph_p2 = new BaseGraph(dir_p2, true, true, 100, 8);  // Création de l'instance de BaseGraph
+        graph_p2.create(500);
+        NodeAccess na_p2 = graph_p2.getNodeAccess();
+        na_p2.setNode(1, 10, 10,0); na_p2.setNode(2, 8, 10,0);
+        na_p2.setNode(3, 8, 7,0); na_p2.setNode(4, 10, 7,0);
+        graph_p2.edge(1,2).setDistance(2);
+        graph_p2.edge(1,3).setDistance(3);
+        graph_p2.edge(3,4).setDistance(5);
+
+        // Init 2 paths
+        Path p1 = new Path(graph_p1);
+        Path p2 = new Path(graph_p2);
+
+        /* Null weights & distances - test will fail
+             Ici, on travaillera seulement avec un path, car on veut systématiquement éviter
+             la ligne qui "fail" si les paths ne sont pas égaux, afin de voir si strictViolations
+             est correctement retourné
+         */
+
+        // Arrange
+        p1.setWeight(0); p1.setDistance(0); p1.setTime(0);
+        p2.setWeight(1.289203); p2.setDistance(11); p2.setTime(11);
+
+        // Act
+
+        // Cas où les 2 paths sont zéro
+        List<String> output1 = comparePaths(p1, p1, 1,4, seed);
+        List<String> expected_output1 = new ArrayList<>();
+        expected_output1.add("path weights cannot be null");
+        expected_output1.add("path distances cannot be null");
+        expected_output1.add("path times cannot be null");
+
+        // Cas où un seul path est null
+        List<String> output2 = comparePaths(p1, p2, 1,4, seed);
+        List<String> expected_output2 = new ArrayList<>();
+        // La méthode ne devrait pas reconnaître que
+        expected_output2.add("one path weight cannot be null");
+        expected_output2.add("one path distance cannot be null");
+        expected_output2.add("one path time cannot be null");
+
+        // Assert
+        assertEquals(expected_output1, output1);
+        assertEquals(expected_output2, output1);
     }
 
     // non valide et faux
